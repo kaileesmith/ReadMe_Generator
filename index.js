c18fee248ff80991390c2619e6e4f83943faf3fa
Kailee Smith <kaileesmith1501@gmail.com>
2020-11-30 21:40:32 -0800

    added intial questions including description, installation instructions, usage information, contribution guidelines, and test instructions

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage of this project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How would you like other to contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What are the test instructions?',
+    },
 ];
 
 // function to write README file
