2ad38385b9d7962c7d52a2586fd6dca03d3a551b
Kailee Smith <kaileesmith1501@gmail.com>
2020-12-07 21:13:18 -0800

    added spacing for markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# 
-  ${data.title} ${data.description} ${data.installation} ${data.usage} ${data.contributing} ${data.testing}  ${data.github} ${data.email}
+  return `# ${data.title} 
+
+  
+  
+  Description
+  ${data.description} 
+  
+  
+  Installation
+  ${data.installation}
+  
+  Usage
+  ${data.usage} 
+  
+  Contributing
+  ${data.contributing} 
+  
+  Testing
+  ${data.testing}  
+
+  License
+  ${data.license}
+  
+  
+  Questions
+
+  If you have any questions please feel free to contact me on github or via email.
+  
+  Github:${data.github}
+
+  Email:${data.email}
 `;
 }
 
